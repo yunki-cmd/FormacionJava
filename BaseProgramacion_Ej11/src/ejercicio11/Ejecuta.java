db7239a3f0a74fb63d56d8ba69001eb745661ea7
yun <yunqiangwu7@gmail.com>
2021-09-21 19:02:36 +0200

    Yun 21-09-2021 19:01 baseProgramacion ejercicio resuelto

---
+++ BaseProgramacion_Ej11/src/ejercicio11/Ejecuta.java
@@ -0,0 +1,17 @@
+package ejercicio11;
+
+public class Ejecuta {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Programa que lee nombre y año de nacimiento de una serie de alumnos hasta introducir
+como nombre de alumno “fin”. En ese momento mostrará la edad media de los alumnos y
+el nombre del alumno menor. NOTA: Al introducir como nombre “fin”, no nos ha de pedir el
+año de nacimiento.
+		 * 
+		 * */
+
+	}
+
+}
