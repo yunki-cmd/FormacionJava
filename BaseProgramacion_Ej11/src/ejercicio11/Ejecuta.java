3d05172e7f0d8a8a370dced4fbbbb36bcc879f2b
yun <yunqiangwu7@gmail.com>
2021-09-21 19:06:18 +0200

    .settings Yun 21-09-2021 19:06 baseProgramacion

--- BaseProgramacion_Ej11/src/ejercicio11/Ejecuta.java
+++ BaseProgramacion_Ej11/src/ejercicio11/Ejecuta.java
@@ -1,5 +1,7 @@
 package ejercicio11;
 
+import java.util.Scanner;
+
 public class Ejecuta {
 
 	public static void main(String[] args) {
@@ -11,7 +13,38 @@ el nombre del alumno menor. NOTA: Al introducir como nombre “fin”, no nos ha de 
 año de nacimiento.
 		 * 
 		 * */
-
+	int año_actual = 2021;
+	double mediaAcu = 0;
+	String nombre;
+	String nombre_menor="";
+	int menor = Integer.MAX_VALUE;
+	int añoNacimiento;
+	boolean salir = false;
+	int contador = 0;
+	Scanner input = new Scanner(System.in);
+	while(!salir) {
+		System.out.println("introduzca su nombre");
+		nombre = input.nextLine();
+		if(nombre.equalsIgnoreCase("fin"))salir=true;
+		if(!salir) {			
+			System.out.println("intorduzca su año de nacimiento "
+					+ "ejemplo : 2000");
+			añoNacimiento = Integer.parseInt(input.nextLine());
+			mediaAcu = (double) mediaAcu + (año_actual-añoNacimiento);
+			contador++;
+			if( (año_actual- añoNacimiento) < menor) {
+				menor = año_actual- añoNacimiento;
+				nombre_menor = nombre;
+			}
+		}
+		
+		
+	}
+	System.out.println("la media de edad es: "+
+			mediaAcu/contador+" y el menor de todos es: "+
+			nombre_menor.toUpperCase()+" con "+ menor
+			);
+	input.close();	
 	}
 
 }
